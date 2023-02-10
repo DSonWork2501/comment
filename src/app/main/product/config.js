5dc1bb14b4e9af1aaebf677e7416cd41002a2eaa
vinhtq24 <vinhtq24>
2023-02-10 11:41:52 +0700

    up code

---
+++ src/app/main/product/config.js
@@ -0,0 +1,15 @@
+
+const ProductConfig = {
+	settings: {
+	},
+	// auth: authRoles.onlyGuest,
+	routes: [
+		{
+			path: '/product',
+			exact: true,
+			component: React.lazy(() => import('.'))
+		}
+	]
+};
+
+export default ProductConfig;
