b4c32331fd0378625b627ead9efcb9790adf5b7c
vinhtq24 <vinhtq24>
2023-03-07 17:05:31 +0700

    up code

--- src/app/main/product/components/product/edit/classify/RightSideContent.js
+++ src/app/main/product/components/product/edit/classify/RightSideContent.js
@@ -1,18 +1,84 @@
 import { Box } from "@material-ui/core"
-import { CmsBoxLine, CmsFormikSearchAutoComplete, CmsFormikTextField } from "@widgets/components"
+import { CmsAutocomplete, CmsBoxLine, CmsFormikTextField } from "@widgets/components"
 import { keyStore } from "app/main/product/common"
 import { getList } from "app/main/product/store/productSlice"
-import React, { useEffect } from "react"
+import { get } from "lodash"
+import React, { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 
-function RightSideContent({ formik, prefix }) {
+function SearchProductSKUContent({ formik, prefix }) {
     const dispatch = useDispatch()
     const entities = useSelector(store => store[keyStore].product.entities)?.data
     const loading = useSelector(store => store[keyStore].product.loading)
 
+    const [sku, setSku] = useState(null)
+
+    const item_product = get(formik.values, prefix)?.item ? get(formik.values, prefix)?.item : null
+    const item_sku = item_product ? item_product?.sku : null
     useEffect(() => {
         dispatch(getList({ homeSubscription: 2, PageNumber: 1, rowsPage: 30 }))
     }, [dispatch])
+
+    useEffect(() => {
+        if (item_sku) {
+            setSku(entities?.find(x => x.sku === item_sku) || null)
+        }
+    }, [entities, item_sku])
+
+    const onChangeProduct = (event, value) => {
+        setSku(value)
+    }
+
+    console.log('item_product', item_product)
+
+    return (
+        <>
+            <CmsAutocomplete
+                loading={loading}
+                label="Danh sách Sản phẩm"
+                value={sku}
+                multiple={false}
+                data={entities || []}
+                onChange={onChangeProduct}
+                required={true}
+                onKeyPress={(value) => { dispatch(getList({ search: value, homeSubscription: 2, PageNumber: 1, rowsPage: 30 })) }}
+                autocompleteProps={{
+                    // limitTags: 20,
+                    getOptionLabel: (option) => option?.name,
+                    // ChipProps: {
+                    //     size: 'small'
+                    // },
+                    size: 'small',
+                    disableCloseOnSelect: false,
+                    filterSelectedOptions: false,
+
+                }}
+                renderOption={(option, props) =>
+                    <Box className="w-full" component="div" style={{ '& > img': { mr: 2, flexShrink: 0 } }}>
+                        <div className="flex justify-between items-center space-x-4">
+                            <div className="flex justify-start">
+                                {option.name}
+                            </div>
+                            <div className="flex justify-end">
+                                {option.image &&
+                                    <img
+                                        loading="lazy"
+                                        width="200"
+                                        src={`${option.image}`}
+                                        srcSet={`${option.image} 2x`}
+                                        alt=""
+                                    />
+                                }
+                            </div>
+                        </div>
+                    </Box>
+                }
+            />
+        </>
+    )
+}
+
+function RightSideContent({ formik, prefix }) {
 
     return (
         <div className="w-full space-y-8">
@@ -21,48 +87,10 @@ function RightSideContent({ formik, prefix }) {
                     <CmsFormikTextField size="small" name={`${prefix}name`} label="Tên" formik={formik} />
                     <CmsFormikTextField isNumberFormat={true} size="small" name={`${prefix}capacity`} label="capacity" formik={formik} />
                     <CmsFormikTextField isNumberFormat={true} size="small" name={`${prefix}heightlimit`} label="height limit" formik={formik} />
+                    <SearchProductSKUContent formik={formik} prefix={prefix} />
                 </div>
             </CmsBoxLine>
-            <CmsFormikSearchAutoComplete
-                name={`${prefix}item`}
-                label="Sản phẩm"
-                onKeyPress={(value) => { dispatch(getList({ search: value, homeSubscription: 2, PageNumber: 1, rowsPage: 30 })) }}
-                loading={loading}
-                multiple={false}
-                required={false}
-                formik={formik}
-                data={entities || []}
-                autocompleteProps={{
-                    limitTags: 20,
-                    getOptionLabel: (option) => option?.type,
-                    ChipProps: {
-                        size: 'small'
-                    },
-                    size: 'small',
-                    disableCloseOnSelect: true,
-                    filterSelectedOptions: false,
-                    renderOption: (option, props) =>
-                        <Box className="w-full" component="div" style={{ '& > img': { mr: 2, flexShrink: 0 } }}>
-                            <div className="flex justify-between items-center space-x-4">
-                                <div className="flex justify-start">
-                                    {option.name}
-                                </div>
-                                <div className="flex justify-end">
-                                    {option.img &&
-                                        <img
-                                            loading="lazy"
-                                            width="200"
-                                            src={`${option.img}`}
-                                            srcSet={`${option.img} 2x`}
-                                            alt=""
-                                        />
-                                    }
-                                </div>
-                            </div>
-                        </Box>,
-                    // PaperComponent: CustomPaper
-                }}
-            />
+
         </div>
     )
 }
