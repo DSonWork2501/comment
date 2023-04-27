a7255b1437319775d94ed56c2e4e4313675bfed6
tao quang vinh <quangvinh2911176@example.com>
2023-04-27 21:22:25 +0700

    up code mới

--- src/app/main/customer-shelf/components/detail/ShelfDetail.js
+++ src/app/main/customer-shelf/components/detail/ShelfDetail.js
@@ -1,11 +1,11 @@
-import { CmsDialog, CmsLabel, } from "@widgets/components"
+import { CmsButton, CmsDialog, CmsLabel, } from "@widgets/components"
 import React, { } from "react"
 import { keyStore } from "../../common"
 import { useSelector } from "react-redux"
 import clsx from "clsx"
 import noImage from '@widgets/images/noImage.jpg';
 import { LabelInfo } from "@widgets/components/common/LabelInfo"
-import { makeStyles } from "@material-ui/core"
+import { Link, makeStyles } from "@material-ui/core"
 export const baseurl = `${process.env.REACT_APP_API_BASE_URL}/product/img/`
 
 const useStyles = makeStyles((theme) => ({
@@ -24,15 +24,19 @@ const useStyles = makeStyles((theme) => ({
 function DetailModelContent({ value }) {
     const classes = useStyles()
     const slots = value?.slots || []
+
     return (
         <div className="w-full grid border-solid border-2 border-green-500 rounded-4 ">
             <div className="w-full shadow-4 bg-green-500 rounded-2 grid">
                 <CmsLabel
                     className="text-white justify-self-center"
-                    content={value?.name}
+                    content={`${value?.name} (Số lượng: ${slots?.length})`}
                 />
             </div>
             <div className="w-full space-y-8">
+                <div>
+
+                </div>
                 {slots.map((item, index) => (
                     <DetailShelfProductContent
                         data={item}
@@ -43,12 +47,28 @@ function DetailModelContent({ value }) {
                 ))}
             </div>
         </div>
+
     )
 }
 
 function ShelfDetailContent({ open, handleClose }) {
     const entities = useSelector(store => store[keyStore]?.cusShelf?.detailEntities)
-    console.log('entities', entities)
+
+    const handleDownloadAll = () => {
+        for (let index = 0; index < entities?.data?.length; index++) {
+            const slots = entities?.data[index].slots;
+            for (let i = 0; i < slots?.length; i++) {
+                const item = slots[i].item;
+                if (item.qrcode) {
+                    var a = document.createElement("a"); //Create <a>
+                    a.href = `data:image/png;base64, ${item.qrcode}`; //Image Base64 Goes here
+                    a.download = `${item.name}_${item.uniqueid?.replace('.', '_')}.png`; //File name Here
+                    a.click(); //Downloaded file
+                }
+            }
+        }
+    }
+
     return (
         <CmsDialog
             title="Thông tin chi tiết tủ"
@@ -56,14 +76,23 @@ function ShelfDetailContent({ open, handleClose }) {
             handleClose={handleClose}
             size="md"
         >
-            <div className="">
-                {entities?.data?.map((item, index) => (
+            <div className="w-full space-y-4">
+                <div className="text-right">
+                    {entities?.data.length > 0 && <CmsButton className="" variant="outlined" size="small" label="Tải All" component={Link} onClick={() => handleDownloadAll()} />}
+                </div>
+                {entities?.data?.length > 0 ? entities?.data?.map((item, index) => (
                     <DetailModelContent
                         value={item}
                         index={index}
                         key={`DetailShelf-${index}`}
                     />
-                ))}
+                )) : <div className="border-collapse border-2 border-green-500">
+                    <CmsLabel
+                        content={'Không có dữ liệu !'}
+                        className="text-red-500 text-center"
+                    />
+                </div>
+                }
             </div>
         </CmsDialog>
     )
@@ -73,6 +102,14 @@ function ShelfDetailContent({ open, handleClose }) {
 function DetailShelfProductContent({ data, index, classes }) {
     const value = data?.item || null
     const img = value.img ? `${baseurl}${value.img}` : noImage
+
+    const handleDownload = ({ qrcode, name, uniqueid }) => {
+        var a = document.createElement("a"); //Create <a>
+        a.href = `data:image/png;base64, ${qrcode}`; //Image Base64 Goes here
+        a.download = `${name}_${uniqueid?.replace('.', '_')}.png`; //File name Here
+        a.click(); //Downloaded file
+    }
+
     return (
         <div
             key={`div-0-detai-${index}`}
@@ -84,15 +121,16 @@ function DetailShelfProductContent({ data, index, classes }) {
             <div className="w-full self-center space-y-16">
                 <LabelInfo key={`uniqueid-${index}-labelInfo`} label={{ content: 'mã', className: 'min-w-min' }} info={{ content: value?.uniqueid || '-' }} />
                 <LabelInfo key={`imei_ord-${index}-labelInfo`} label={{ content: 'imei đơn hàng', className: 'min-w-min' }} info={{ content: value?.imei_ord || '-' }} />
-              
+
             </div>
             <div className="w-full self-center space-y-16">
                 <LabelInfo key={`name-${index}-labelInfo`} label={{ content: 'tên', className: 'min-w-min' }} info={{ content: value?.name || '-' }} />
                 {/* <LabelInfo key={`color-${index}-labelInfo`} label={{ content: 'màu', className: 'min-w-min' }} info={{ content: value?.color || '-' }} /> */}
                 <LabelInfo key={`sku-${index}-labelInfo`} label={{ content: 'sku', className: 'min-w-min' }} info={{ content: value?.sku || '-' }} />
             </div>
-            <div className="w-1/5 self-center space-y-16">
-                <img alt={`qrcord_${index}`} src={value.qrcode ? `data:image/png;base64, ${value.qrcode}` : noImage} className="h-64" />
+            <div className="w-1/5 self-center space-y-2 text-center">
+                <img alt={`qrcord_${index}`} src={value.qrcode ? `data:image/png;base64, ${value.qrcode}` : noImage} className="" />
+                {value.qrcode && <CmsButton variant="outlined" size="small" label="tải về" component={Link} onClick={() => handleDownload(value)} />}
             </div>
         </div>
     )
--- src/app/main/customer-shelf/index.js
+++ src/app/main/customer-shelf/index.js
@@ -2,7 +2,7 @@ import withReducer from "app/store/withReducer";
 import React, { useCallback, useState } from "react";
 import { keyStore } from "./common";
 import reducer from "./store";
-import { CmsCardedPage, CmsIconButton, CmsTableBasic } from "@widgets/components";
+import { CmsButton, CmsCardedPage, CmsIconButton, CmsTableBasic } from "@widgets/components";
 import { getShelf, getWine, setSearch } from "./store/customerShelfSlice";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,6 +13,7 @@ import GenFilterOptionContent from './components/index/GenFilterOption'
 import { LabelInfo } from "@widgets/components/common/LabelInfo";
 import ShelfDetailContent from "./components/detail/ShelfDetail";
 import { CustomerProductType } from "./model/CustomerProductType";
+import { Link } from "@material-ui/core";
 
 
 export const baseurl = `${process.env.REACT_APP_API_BASE_URL}/product/img/`
@@ -33,6 +34,15 @@ function CustomerShelfContent() {
         dispatch(getWine({ cusId: cusid, parentId: id, cms: 1 }))
     }, [dispatch])
 
+    const handleDownloadQRCode = useCallback(({qrcode, name, uniqueid}) => {
+        var a = document.createElement("a"); //Create <a>
+        a.href = `data:image/png;base64, ${qrcode}`; //Image Base64 Goes here
+        a.download = `${name}_${uniqueid?.replace('.', '_')}.png`; //File name Here
+        a.click(); //
+    }, [])
+
+
+
     const data = useMemo(() =>
         entities?.data?.map((x, index) => ({
             ...x,
@@ -40,22 +50,26 @@ function CustomerShelfContent() {
                 <LabelInfo label={{ content: 'SKU' }} info={{ content: x.sku }} />
                 <LabelInfo label={{ content: 'Unique ID' }} info={{ content: x.uniqueid }} />
             </div>,
-            qrcode: <img alt={`image_${index}`} src={x.qrcode ? `data:image/png;base64, ${x.qrcode}` : noImage} className="h-64" />,
+            qrcode:
+                <div className="space-y-4 w-60">
+                    <img alt={`image_${index}`} src={x.qrcode ? `data:image/png;base64, ${x.qrcode}` : noImage} className="text-center" />
+                    {x.qrcode && <CmsButton className="text-center" variant="outlined" size="small" label="tải về" component={Link} onClick={() => handleDownloadQRCode(x)} />}
+                </div>,
             image: <img alt={`image_${index}`} src={x.img ? `${baseurl}${x.img}` : noImage} className="h-64" />,
             action: <div>
                 {x.parentid === 0 && <CmsIconButton icon="info" onClick={() => handleShowDetail(x.cusid, x.id)} tooltip={'chi tiết'} />}
             </div>
         })) || []
-        , [entities, handleShowDetail])
+        , [entities, handleShowDetail, handleDownloadQRCode])
 
-        const columns = [
-            new initColumn({ field: 'id', label: CustomerProductType['wine'].id === search.Type ? 'imei': 'id', classHeader: "w-128", sortable: false }),
-            new initColumn({ field: "info", label: "Thông tin", alignHeader: "left", alignValue: "left", sortable: false }),
-            new initColumn({ field: "name", label: "Tên sản phẩm", alignHeader: "left", alignValue: "left", sortable: false }),
-            new initColumn({ field: "cusname", label: "Tên khách hàng", alignHeader: "left", alignValue: "left", sortable: false }),
-            CustomerProductType['wine'].id === search.Type && new initColumn({ field: "qrcode", label: "QRCode", alignHeader: "left", alignValue: "left", sortable: false }),
-            new initColumn({ field: "image", label: "Hình ảnh", alignHeader: "left", alignValue: "left", sortable: false }),
-        ]
+    const columns = [
+        new initColumn({ field: 'id', label: CustomerProductType['wine'].id === search.Type ? 'imei' : 'id', classHeader: "w-128", sortable: false }),
+        new initColumn({ field: "info", label: "Thông tin", alignHeader: "left", alignValue: "left", sortable: false }),
+        new initColumn({ field: "name", label: "Tên sản phẩm", alignHeader: "left", alignValue: "left", sortable: false }),
+        new initColumn({ field: "cusname", label: "Tên khách hàng", alignHeader: "left", alignValue: "left", sortable: false }),
+        CustomerProductType['wine'].id === search.Type && new initColumn({ field: "qrcode", label: "QRCode", alignHeader: "left", alignValue: "left", sortable: false }),
+        new initColumn({ field: "image", label: "Hình ảnh", alignHeader: "left", alignValue: "left", sortable: false }),
+    ]
 
     return (
         <CmsCardedPage
