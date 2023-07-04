ee9285f606cfa17e769a5ba7b30ef989514cd3a5
unknown <manhtc3@fpt.com.vn>
2023-07-04 23:17:05 +0700

    p code

--- src/app/main/order/common.js
+++ src/app/main/order/common.js
@@ -3,6 +3,34 @@ import React from "react";
 
 export const keyStore = "orders"
 export const keyI18n = "i18nOrder"
+
+export const btnStatus = [
+    {
+        status: 2,
+        name: "Xác nhận"
+    },
+    {
+        status: 6,
+        name: "Đóng gói"
+    },
+    {
+        status: 5,
+        name: "Thanh toán"
+    },
+    {
+        status: 3,
+        name: "Xác nhận thanh toán"
+    },
+    {
+        status: 4,
+        name: "Hoàn thành"
+    },
+    {
+        status: 0,
+        name: "Hủy"
+    },
+];
+
 export const links = (valueOb) => orderStatusArray.map(val => (
     {
         id: val.id, name: val.name, link: "/order/" + val.id, otherComp: <div className={val.className} style={{
