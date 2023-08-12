2b2e97468e2923b49836a22e9ef10fb20818123c
unknown <manhtc3@fpt.com.vn>
2023-08-12 14:53:18 +0700

    p code

--- src/app/main/order/components/index/index.js
+++ src/app/main/order/components/index/index.js
@@ -146,7 +146,7 @@ function OrderView() {
             ? (summary?.da_huy + summary?.da_tao + summary?.da_xac_nhan + summary?.da_dong_goi + summary?.cho_thanh_toan + summary?.da_thanh_toan + summary?.hoan_tat + summary?.van_chuyen)?.toLocaleString('en-US')
             : 0,
         //5: summary?.cho_thanh_toan ? summary?.cho_thanh_toan?.toLocaleString('en-US') : 0,
-        // 4: summary?.hoan_tat ? summary?.hoan_tat?.toLocaleString('en-US') : 0,
+        4: summary?.hoan_tat ? summary?.hoan_tat?.toLocaleString('en-US') : 0,
         // 3: summary?.da_thanh_toan ? summary?.da_thanh_toan?.toLocaleString('en-US') : 0,
         2: summary?.da_xac_nhan ? summary?.da_xac_nhan?.toLocaleString('en-US') : 0,
         1: summary?.da_tao ? summary?.da_tao?.toLocaleString('en-US') : 0,
