f33b9a3be793933eecd984570062da556933dfd0
unknown <User@WFPLBE00225846.officesg.local>
2023-10-19 17:23:15 +0700

    p code

--- src/app/main/payoo/index.js
+++ src/app/main/payoo/index.js
@@ -7,7 +7,28 @@ const Payoo = () => {
 
     useEffect(() => {
         if (code) {
-            Connect.live.payoo.getList({ id: code })
+            Connect.live.payoo.getList({ id: code }).then((val) => {
+                const res = val.data?.result
+                if (res) {
+                    // Find the element by its class names
+                    const element = document.querySelector('.MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-textPrimary');
+
+                    // Check if the element exists
+                    if (element) {
+                        // Create a click event
+                        const clickEvent = new MouseEvent('click', {
+                            bubbles: true,
+                            cancelable: true,
+                            view: window,
+                        });
+
+                        // Dispatch the click event on the element
+                        element.dispatchEvent(clickEvent);
+                    } else {
+                        console.log('Element not found');
+                    }
+                }
+            })
         }
     }, [code])
 
